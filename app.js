ff65917f3d6fb8de56be13c280d6a93fa3b75e00
mikesternk <mikesternk@gmail.com>
2024-02-19 19:19:33 -0600

    Getting student grades

--- app.js
+++ app.js
@@ -35,6 +35,7 @@
 // console.log(courseInfo);
 // console.log("Hello World");
 
+
 /**************** Provided Information ****************/
 
 // The provided course information.
@@ -116,32 +117,59 @@ const LearnerSubmissions = [
 ];
 
 function getLearnerData(course, ag, submissions) {
-const results = [];
+    const results = [];
 
-const example_result = [
-    {
-    id: 125,
-    avg: 0.985, // (47 + 150) / (50 + 150)
-    1: 0.94, // 47 / 50
-    2: 1.0, // 150 / 150
-    },
-    {
-    id: 132,
-    avg: 0.82, // (39 + 125) / (50 + 150)
-    1: 0.78, // 39 / 50
-    2: 0.833, // late: (140 - 15) / 150
-    },
-];
+    const example_result = [
+        {
+            id: 125,
+            avg: 0.985, // (47 + 150) / (50 + 150)
+            1: 0.94, // 47 / 50
+            2: 1.0, // 150 / 150
+        },
+        {
+            id: 132,
+            avg: 0.82, // (39 + 125) / (50 + 150)
+            1: 0.78, // 39 / 50
+            2: 0.833, // late: (140 - 15) / 150
+        },
+    ];
 
-// Parse submission data.
-console.log(`Submission Data:`, submissions );
-// Check to see if the submission was late; if so, deduct 10% of the maximum possible points.
-// Find existing data for this learner, if any.
-// If the learner already has data, add the new score to the existing data.
-// Calculate the average score for each learner and remove the extra data.
+    // Parse submission data.
+    console.log(`Submission Data:`, submissions );
+    // Check to see if the submission was late; if so, deduct 10% of the maximum possible points.
+    // Find existing data for this learner, if any.
+    // If the learner already has data, add the new score to the existing data.
+    // Calculate the average score for each learner and remove the extra data.
 
-//==== PUT CODE HERE =====//
-return results;
+    //==== PUT CODE HERE =====//
+
+    // Function to group submissions by learner ID
+    function groupSubmissionsByLearnerId(submissions) {
+        const groupedAssignments = {};
+        
+        submissions.forEach(({ learner_id, assignment_id, submission}) => {
+            if (!groupedAssignments[learner_id]) {
+                groupedAssignments[learner_id] = [];
+            }
+
+            groupedAssignments[learner_id].push({ assignment_id, score: submission.score });
+        });
+
+        totalGrades.forEach ((assignments) => {
+            if (!groupedAssignments[assignments]) {
+                groupedAssignments[assignments] = [];
+            }
+
+            groupedAssignments[assignments].push({ assignment_id, score: submission.score });
+        });
+
+        return groupedAssignments;
+    }
+
+    let theAssignments = groupSubmissionsByLearnerId(LearnerSubmissions, AssignmentGroup);
+    console.log(theAssignments);
+
+    return results;
 }
 
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
@@ -153,7 +181,8 @@ function average(grade, total) {
     let gradeID, totalPoints;
 
     console.log(`This is the course info: ${total.course_id}`);
-
+    
+    // Trying to use a for loop to get students grades
     // for (let i = 0; i < grade.length; i++) {
     //     console.log(`Student ID: ${grade[i].learner_id}`)
     //     console.log(`Assignment ID: ${grade[1].assignment_id}`)
@@ -174,25 +203,6 @@ function average(grade, total) {
     //     }
     // }
 
-// Function to group submissions by learner ID
-function groupSubmissionsByLearnerId(submissions, totalGrades) {
-    const groupedAssignments = {};
-    
-    submissions.forEach(({ learner_id, assignment_id, submission}) => {
-        if (!groupedAssignments[learner_id]) {
-            groupedAssignments[learner_id] = [];
-        }
-        
-        totalGrades.forEach(submission)
-        groupedAssignments[learner_id].push({ assignment_id, score: submission.score });
-    });
-    
-    return groupedAssignments;
-}
-
-let theAssignments = groupSubmissionsByLearnerId(LearnerSubmissions, AssignmentGroup);
-
-console.log(theAssignments)
 
     // make sure the assignment ID is correct   
     if (gradeID == totalPoints) {
